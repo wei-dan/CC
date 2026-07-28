d0e7962a72c6fe34d0ea38fe21109c3f0ba836f3
wei-dan <weidan358@gmail.com>
2026-07-28 17:52:15 +0800

    feat: 添加图片分析工具并重构代理调用

--- Program.cs
+++ Program.cs
@@ -3,16 +3,10 @@ using Microsoft.Extensions.AI;
 using OpenAI;
 using OpenAI.Chat;
 using System;
-using System.Buffers.Text;
 using System.ClientModel;
 using System.ComponentModel;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Runtime.InteropServices;
-using System.Threading.Tasks;
 
-string apiKey = "sk-ws-H.EIIMERL.YOZt.MEUCIEnJYx_DqGa8aGadlD1AzkUXKik4SYqkIaYjstnlNHpcAiEA40Q3ru1LsKM_OQ_HO32SbYCnl-M7lWgJhTWkIk3K5l0"; //"sk-c7366fcac5aa4023827e049e7a714705";
+const string apiKey = "sk-ws-H.EIIMERL.YOZt.MEUCIEnJYx_DqGa8aGadlD1AzkUXKik4SYqkIaYjstnlNHpcAiEA40Q3ru1LsKM_OQ_HO32SbYCnl-M7lWgJhTWkIk3K5l0"; //"sk-c7366fcac5aa4023827e049e7a714705";
 AIAgent agent = new OpenAIClient(
         new ApiKeyCredential(apiKey),
         new OpenAIClientOptions
@@ -21,7 +15,9 @@ AIAgent agent = new OpenAIClient(
         }
     )
     .GetChatClient("qwen3.7-plus") //("deepseek-v4-pro")
-    .AsAIAgent();
+    .AsAIAgent(tools: [
+        AIFunctionFactory.Create(AnalyzePicture)
+        ]);
 AgentSession session = await agent.CreateSessionAsync();
 
 while (true)
@@ -34,21 +30,36 @@ while (true)
         continue;
     }
 
-    var message = new Microsoft.Extensions.AI.ChatMessage(
-    ChatRole.User,
-    new AIContent[]
-    {
-        new TextContent(
-            "图中描绘的是什么景象?"
-        ),
-
-        new UriContent(
-            new Uri(
-            "https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20241022/emyrja/dog_and_girl.jpeg"),
-            "image/jpeg")
-    });
-
-    var response = await agent.RunAsync(message, session);
+    var response = await agent.RunAsync(input, session);
 
     Console.WriteLine($"Bot: { response.Text }");
+}
+
+
+[Description("Analyzes a picture based on the provided description and URL.")]
+static string AnalyzePicture(string description, string url)
+{
+    AIAgent agent1 = new OpenAIClient(
+        new ApiKeyCredential(apiKey),
+        new OpenAIClientOptions
+        {
+            Endpoint = new Uri("https://dashscope.aliyuncs.com/compatible-mode/v1")
+        }
+    )
+    .GetChatClient("qwen3.7-plus") //("deepseek-v4-pro")
+    .AsAIAgent();
+
+    var mes = new Microsoft.Extensions.AI.ChatMessage(
+        ChatRole.User,
+        new AIContent[]
+        {
+            new TextContent(
+                description
+            ),
+            new UriContent(
+                new Uri(url),
+                "image/jpeg")
+        });
+    var response = agent1.RunAsync(mes).GetAwaiter().GetResult();
+    return response.Text;
 }
