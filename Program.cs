80f4cee8d36e6344b5e093480b7d0ced544c94ed
wei-dan <weidan358@gmail.com>
2026-07-28 17:21:15 +0800

    refactor: 重构为固定图文多模态请求并移除截图功能

--- Program.cs
+++ Program.cs
@@ -1,57 +1,54 @@
 using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
 using OpenAI;
 using OpenAI.Chat;
 using System;
+using System.Buffers.Text;
 using System.ClientModel;
+using System.ComponentModel;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 
-string apiKey = "sk-c7366fcac5aa4023827e049e7a714705";
+string apiKey = "sk-ws-H.EIIMERL.YOZt.MEUCIEnJYx_DqGa8aGadlD1AzkUXKik4SYqkIaYjstnlNHpcAiEA40Q3ru1LsKM_OQ_HO32SbYCnl-M7lWgJhTWkIk3K5l0"; //"sk-c7366fcac5aa4023827e049e7a714705";
 AIAgent agent = new OpenAIClient(
         new ApiKeyCredential(apiKey),
         new OpenAIClientOptions
         {
-            Endpoint = new Uri("https://api.deepseek.com")
-        })
-        .GetChatClient("deepseek-v4-pro")
-        .AsAIAgent(
-            tools: [
-
-            ]);
+            Endpoint = new Uri("")
+        }
+    )
+    .GetChatClient("qwen3.7-plus") //("deepseek-v4-pro")
+    .AsAIAgent();
 AgentSession session = await agent.CreateSessionAsync();
+
 while (true)
 {
     Console.Write("You: ");
     string? input = Console.ReadLine();
 
-    // 当输入流结束（EOF）时退出循环
     if (input is null)
     {
-        break;
-    }
-
-    if (input == "/screenshot")
-    {
-        var imageBytes = CaptureScreen();
-        File.WriteAllBytes("screenshot.png", imageBytes);
-        Console.WriteLine("Bot: 截图已保存为 screenshot.png");
         continue;
     }
 
-    var response = await agent.RunAsync(input, session);
-    Console.WriteLine($"Bot: { response.Text }");
-}
+    var message = new Microsoft.Extensions.AI.ChatMessage(
+    ChatRole.User,
+    new AIContent[]
+    {
+        new TextContent(
+            "图中描绘的是什么景象?"
+        ),
 
-// 请确保在 .csproj 中引用 System.Drawing.Common 包并设置 <UseWindowsForms>true</UseWindowsForms>
-static byte[] CaptureScreen()
-{
-    var bounds = Screen.PrimaryScreen.Bounds;
-    using var bitmap = new Bitmap(bounds.Width, bounds.Height);
-    using var graphics = Graphics.FromImage(bitmap);
-    graphics.CopyFromScreen(bounds.X, bounds.Y, 0, 0, bounds.Size);
-    using var ms = new MemoryStream();
-    bitmap.Save(ms, ImageFormat.Png);
-    return ms.ToArray();
+        new UriContent(
+            new Uri(
+            "https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20241022/emyrja/dog_and_girl.jpeg"),
+            "image/jpeg")
+    });
+
+    var response = await agent.RunAsync(message, session);
+
+    Console.WriteLine($"Bot: { response.Text }");
 }
