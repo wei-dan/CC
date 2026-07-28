98f7ea90b6c28da479c4b0f20ee39aa39eca4095
wei-dan <weidan358@gmail.com>
2026-07-28 15:29:49 +0800

    feat: 集成DeepSeek AI代理以提供智能回复

--- Program.cs
+++ Program.cs
@@ -1,5 +1,22 @@
+using Microsoft.Agents.AI;
+using OpenAI;
+using OpenAI.Chat;
 using System;
+using System.ClientModel;
 
+string apiKey = "sk-c7366fcac5aa4023827e049e7a714705";
+AIAgent agent = new OpenAIClient(
+        new ApiKeyCredential(apiKey),
+        new OpenAIClientOptions
+        {
+            Endpoint = new Uri("https://api.deepseek.com")
+        })
+        .GetChatClient("deepseek-v4-pro")
+        .AsAIAgent(
+            tools: [
+
+            ]);
+AgentSession session = await agent.CreateSessionAsync();
 while (true)
 {
     Console.Write("You: ");
@@ -10,6 +27,6 @@ while (true)
     {
         break;
     }
-
-    Console.WriteLine("Bot: hello");
+    var response = await agent.RunAsync(input, session);
+    Console.WriteLine($"Bot: { response.Text }");
 }
